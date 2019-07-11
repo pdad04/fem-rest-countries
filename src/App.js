262a51b8a0fa74bebc2c1ceef8a67f26c0dd4f35
Andre <andre_dean@me.com>
2019-07-10 21:38:47 -0700

    Add functionality to load all countries upon initial page load. Ability to filter based on region, and ability to perform search for a country.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,60 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
 import Details from './components/Details';
 
 function App() {
+
+  const [countries, setCountries] = useState()
+  let [fetchedCountries, setFetched] = useState(false);
+
+  useEffect(() => {
+
+    async function fetchAllCountries(){
+      const result = await fetch('https://restcountries.eu/rest/v2/all');
+      const resultJSON = await result.json();
+      setCountries(resultJSON);
+      setFetched(true);
+
+    }
+
+    fetchAllCountries();
+  }, []);
+
+  async function fetchRegion(e){
+    const result = await fetch(`https://restcountries.eu/rest/v2/region/${e.target.innerHTML}`)
+    const resultJSON = await result.json();
+    setCountries(resultJSON);
+  }
+
+  async function fetchCountryByName(e){  
+    if(e.target.value.length > 0){
+      const result = await fetch(`https://restcountries.eu/rest/v2/name/${e.target.value}`)
+      const resultJSON = await result.json();
+      setCountries(resultJSON);
+    }
+  }
+
+  if(fetchedCountries){
   return (
     <div className="App">
       <Header />
-      <Home />
+      <Home 
+        countries={countries}
+        fetchRegion={() => fetchRegion}
+        getTextInput={() => fetchCountryByName}
+      />
       {/* <Details /> */}
     </div>
   );
+  }else{
+    return (
+      <div className="App">
+        <Header />
+      </div>
+    )
+  }
 }
 
 export default App;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,23 +1,43 @@
 import React from 'react';
 import './styles/Home.css';
 
-function Home() {
+function Home(props) {
     return (
         <section className="home-container">
             <div className="search">
-                <input type="text" placeholder="Search for country" className="country-search dark-mode-elements"></input>
+                <input 
+                    type="text" 
+                    placeholder="Search for country" 
+                    className="country-search dark-mode-elements"
+                    onChange={props.getTextInput()}
+                 />
             </div>
             <div className="dropdown">
-                <button class="dropbtn dark-mode-elements">Filter By Region</button>
+                <button className="dropbtn dark-mode-elements">Filter By Region</button>
                 <div className="dropdown-content dark-mode-elements">
-                    <a href="#">Africa</a>
-                    <a href="#">America</a>
-                    <a href="#">Asia</a>
-                    <a href="#">Europe</a>
-                    <a href="#">Oceania</a>
+                    <div onClick={props.fetchRegion()}>Africa</div>
+                    <div onClick={props.fetchRegion()}>Americas</div>
+                    <div onClick={props.fetchRegion()}>Asia</div>
+                    <div onClick={props.fetchRegion()}>Europe</div>
+                    <div onClick={props.fetchRegion()}>Oceania</div>
                 </div>
             </div>
-            <div className="home-countries dark-mode-elements">
+            {props.countries.map(country => (
+                <div className="home-countries dark-mode-elements" key={country.name}>
+                    <div className="country-flag">
+                        <div className="img" style={{backgroundImage: `url(${country.flag})`}}>
+                        </div>
+                         {/* <img src={country.flag} alt={`${country.name} flag`} /> */}
+                    </div>
+                    <div className="home-countries-details">
+                        <h3>{country.name}</h3>
+                        <p>Population: {country.population}</p>
+                        <p>Region: {country.region}</p>
+                        <p>Capital: {country.capital}</p>
+                    </div>
+                </div>
+            ))}
+             {/* <div className="home-countries dark-mode-elements">
                 <img src="https://restcountries.eu/data/usa.svg"></img>
                 <div className="home-countries-details">
                     <h3>United States of America</h3>
@@ -31,7 +51,7 @@ function Home() {
             <div className="home-countries dark-mode-elements">4</div>
             <div className="home-countries dark-mode-elements">5</div>
             <div className="home-countries dark-mode-elements">6</div>
-            <div className="home-countries dark-mode-elements">7</div>
+            <div className="home-countries dark-mode-elements">7</div>  */}
         </section>
     )
 }
